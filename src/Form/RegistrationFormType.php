03ac88cdf11c8059b4a6398f42347e378eb361d4
olivier-chatelin <olivier.chatelin@gmail.com>
2021-12-29 01:16:19 +0100

    Add Sidebar

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,7 +18,11 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
+            ->add('firstname',TextType::class,[
+                'attr'=> [
+                    'class' => 'form-control'
+                ],
+            ])
             ->add('lastname')
             ->add('email')
             ->add('plainPassword', PasswordType::class, [
