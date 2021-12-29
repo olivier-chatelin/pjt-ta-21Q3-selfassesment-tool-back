51037aa8c97d1330bb16135b5aaf757ebbbb086e
olivier-chatelin <olivier.chatelin@gmail.com>
2021-12-29 02:40:24 +0100

    Super Admin can manage roles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,5 +24,38 @@ class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+    /**
+     * @Route("/privileges", name="privileges")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function privileges(UserRepository $repository): Response
+    {
+        $users = $repository->findAll();
+        $keyToRemove = 0;
+        foreach ($users as $key=>$user) {
+            if($user->getUserIdentifier() === $this->getUser()->getUserIdentifier()) {
+                $keyToRemove = $key;
+            }
+        }
+        unset($users[$keyToRemove]);
+        return $this->render('admin/privileges.html.twig', [
+            'users' => $users,
+        ]);
+    }
+    /**
+     * @Route("/privileges/update/{id}", name="privileges_update")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function privilegesUpdate(User $user, EntityManagerInterface $entityManager): Response
+    {
+        if (in_array('ROLE_ADMIN',$user->getRoles())) {
+            $user->setRoles(['ROLE_USER']);
+        } else {
+            $user->setRoles(['ROLE_ADMIN']);
+        }
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return $this->redirectToRoute('admin_privileges');
     }
 }
