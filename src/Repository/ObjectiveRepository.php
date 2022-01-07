d50ecfbd06641ca5529c003a3b2f6fcfeefeb4f2
olivier-chatelin <olivier.chatelin@gmail.com>
2022-01-07 14:32:44 +0100

    Made objective CRUD

--- src/Repository/ObjectiveRepository.php
+++ src/Repository/ObjectiveRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Checkpoint;
 use App\Entity\Objective;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -19,22 +20,19 @@ class ObjectiveRepository extends ServiceEntityRepository
         parent::__construct($registry, Objective::class);
     }
 
-    // /**
-    //  * @return Objective[] Returns an array of Objective objects
-    //  */
-    /*
-    public function findByExampleField($value)
+     /**
+      * @return Objective[] Returns an array of Objective objects
+      */
+    public function findOrderedByNumber(Checkpoint $checkpoint): array
     {
         return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('o.id', 'ASC')
-            ->setMaxResults(10)
+            ->andWhere('o.checkpoint = :checkpoint')
+            ->setParameter('checkpoint', $checkpoint)
+            ->orderBy('o.number', 'ASC')
             ->getQuery()
             ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Objective
